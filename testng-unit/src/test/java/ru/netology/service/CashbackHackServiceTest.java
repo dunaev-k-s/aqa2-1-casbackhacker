80e80d69f0a27249a5135bee1b42435b51999e3c
Konstantin Dunaev <shop.overled@gmail.com>
2022-02-12 19:40:06 +0300

    Add tests

--- testng-unit/src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ testng-unit/src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -19,4 +19,24 @@ public class CashbackHackServiceTest {
 
         assertEquals(expected, actual);
     }
+    @Test
+    public void shouldTestBoundAmount() {
+        int amount = 1000;
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void shouldTestNullAmount() {
+        int amount = 0;
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(amount);
+        int expected = 1000;
+
+        assertEquals(expected, actual);
+    }
 }
