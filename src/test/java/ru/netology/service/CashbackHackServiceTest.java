e48f88a94d2eacb021f24d197e13dcc482deff68
Konstantin Dunaev <shop.overled@gmail.com>
2022-02-13 17:33:28 +0300

    Добавил тесты

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -7,6 +7,33 @@ import static org.junit.Assert.*;
 public class CashbackHackServiceTest {
 
     @Test
-    public void remain() {
+    public void shouldTestRegularAmount() {
+        int amount = 1800;
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(amount);
+        int expected = 200;
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void shouldTestBoundAmount() {
+        int amount = 1000;
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void shouldTestNullAmount() {
+        int amount = 0;
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(amount);
+        int expected = 1000;
+
+        assertEquals(expected, actual);
     }
 }
